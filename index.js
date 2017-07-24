d4ce95b0068ea9bd89642b80dd3a949f7684c5f7
gebaoyuan <wangwenchen@e-eduspace.com>
2017-07-24 19:19:59 +0800

    第二版出现啦

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ import geCode from './src/index';
 
 new Vue({
   el: '#app',
-  template: '<ge-code :config="config" class="btn"/>',
+  template: `
+	<div>
+		<ge-code :config="config" class="btn" ref="geCode"/>
+		<button @click=closeCode>
+			关闭倒计时
+		</button>
+	</div>
+
+  `,
   components:{
   	geCode
   },
@@ -18,6 +26,8 @@ new Vue({
 	      totalTime: 60,
 	      //验证码每次隔多久变一次(秒)
 	      tickTime: 1,
+	      //倒计时执行期添加的class 默认 isRun
+	      activeClass:'isRun',
 	      //自定义倒计时期间文本的显示内容
 	      computeText(num){
 	        return '重新获取 ' + num + 's';
@@ -31,8 +41,29 @@ new Vue({
 	      todo: () => {
 	        //这里写验证码的获取程序
 	        console.log('发送验证码')
-	      }
+	      },
+	      //发送完成后的回调
+	      endCallback(type,config){
+	      	console.log('发送完成后的回调',type,config);
+
+	      },
+	      //每次点击都会被调用
+	      click(isRun){
+	      	console.log(`当前验证码状态是:${isRun ? '发送中' : '可发送'}状态`)
+	      },
+	      //是否可用   m默认ture
+	      canUse:true,
+	      //不可用时的类名  默认no-can
+	      noCanClass:'no-can',
+	      //强行停止倒计时 到可用状态
+	      // stop:
 	    }
+  	}
+  },
+  methods:{
+  	closeCode(){
+  		console.log('guanbi');
+  		this.$refs.geCode.stop(1);
   	}
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-  export default {
+  module.exports =  {
    template:`
     <div 
-      :class="{isRun:isRun}"
+      :class="activeClass"
       @click="runtime">
       {{text}}
     </div>
@@ -19,29 +19,32 @@
       }
     },
     mounted(){
-      this.firstText = this.config.startText;
+      this.firstText = this.config.startText || '获取验证码';
       this.totalTime = this.config.totalTime;
       this.tickTime = this.config.tickTime;
     },
     methods: {
       runtime(){
         let config = this.config;
-        if (!this.isRun && (!config.canTodo || config.canTodo())) {
+        config.click && config.click(this.isRun);
+        if (config.canUse && !this.isRun && (!config.canTodo || config.canTodo())) {
 
           this.isFirst = false;
           this.isRun = true;
           this.time = this.totalTime;
-          config.todo && config.todo();
+          config.todo && config.todo(config);
           this.timer = setInterval(() => {
             if (this.time == 0) {
-              this.stop();
+              this.stop(0);
             } else {
               this.time -= this.tickTime;
             }
           }, 1000 * this.tickTime);
         }
       },
-      stop(){
+      stop(type){
+        let config = this.config;
+        config.endCallback && config.endCallback(type,config);
         this.isRun = false;
         clearInterval(this.timer);
       }
@@ -60,6 +63,11 @@
             return this.config.endText || this.firstText;
           }
         }
+      },
+      activeClass(){
+        let config = this.config;
+        return (config.canUse !== undefined && !config.canUse ? (config.noCanClass || 'no-use' ) : "" )
+         + (this.isRun ? (config.activeClass || 'isRun') : '');
       }
     },
     watch: {

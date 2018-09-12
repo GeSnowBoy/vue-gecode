1d52b0b8881e4632400afe6fa5af3ab93e1d3712
葛宝远 <yuansnowboy@163.com>
2018-09-12 14:49:28 +0800

    第4版升级

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// import GeCode from 'ge-test';
 import GeCode from './vue-gecode.vue';
-// import GeCode from './vue-countTo.vue';
 export default GeCode;
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.component('ge-code', GeCode);
